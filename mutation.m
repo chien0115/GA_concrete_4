9f75fd861f3bcee265915cbbc83d5a2c291638a9
chien0115 <133206218+chien0115@users.noreply.github.com>
2024-11-13 19:30:23 +0800

    mutation

--- mutation.m
+++ mutation.m
@@ -9,31 +9,30 @@ function [Y, dispatch_times_new2] = mutation(P, t, dispatch_times)
     A1 = P(r1, 1:y1); % 取出選中的染色體 (派遣順序)
     dispatch_times1 = dispatch_times(r1, :); % 取出對應的派遣時間
 
+    % 定義派遣順序的三個突變位置
+    pos = randperm(y1, 3); % 隨機選擇三個不同的位置
 
-    random_number1 = rand();
-    random_number2 = rand();
+    % 交換所選三個位置的值，這裡可以做三點交換
+    % 比如：將pos1的值給pos2，pos2的值給pos3，pos3的值給pos1
+    temp = A1(pos(1));
+    A1(pos(1)) = A1(pos(2));
+    A1(pos(2)) = A1(pos(3));
+    A1(pos(3)) = temp;
 
-    % 定義派遣順序的突變位置（無奇數位置限制）
-    pos1=ceil(random_number1*y1);
-    pos2=ceil(random_number2*y1);
-    % 交換所選位置的值（調整派遣順序）
-    
-    while A1(pos1) == A1(pos2)
-    % 如果相同，隨機選擇新的位置
-    pos2 = ceil(rand() * y1); % 使用 rand() 隨機生成新的 pos2
-    end
-    
-    A1([pos1, pos2]) = A1([pos2, pos1]);
-
-    A2=A1;
-    % 同時對派遣時間進行突變
-    if t >= 2
-        pos_dispatch = randperm(t, 2); % 隨機選擇兩個不同的位置
-        dispatch_times1([pos_dispatch(1), pos_dispatch(2)]) = dispatch_times1([pos_dispatch(2), pos_dispatch(1)]); 
+    % 同樣對派遣時間進行三點變異
+    if t >= 3
+        pos_dispatch = randperm(t, 3); % 隨機選擇三個不同的位置
+        % 交換所選位置的派遣時間值
+        temp_time = dispatch_times1(pos_dispatch(1));
+        dispatch_times1(pos_dispatch(1)) = dispatch_times1(pos_dispatch(2));
+        dispatch_times1(pos_dispatch(2)) = dispatch_times1(pos_dispatch(3));
+        dispatch_times1(pos_dispatch(3)) = temp_time;
     end
 
     % 返回新的突變後的染色體和派遣時間
-    Y = A2; % 返回突變後的染色體
+    Y = A1; % 返回突變後的染色體
     dispatch_times_new2 = dispatch_times1; % 返回突變後的派遣時間
-
+    % 顯示變異後的派遣時間
+    disp('變異後的派遣時間：');
+    disp(dispatch_times_new2);
 end
